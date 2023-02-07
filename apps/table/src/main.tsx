d42c8bed84f4b71025b891370d07bee5a7984eef
j-w-park <okml133@gmail.com>
2023-02-07 16:23:35 +0900

    chore(table): setup react-query

--- apps/table/src/main.tsx
+++ apps/table/src/main.tsx
@@ -1,13 +1,16 @@
 import { css, Global } from '@emotion/react';
+import { QueryClientProvider } from '@tanstack/react-query';
 import styles from '@table/assets/index.css?raw';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { css, Global } from '@emotion/react';
+import { queryClient } from '@table/queries';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Global styles={css(styles)} />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
